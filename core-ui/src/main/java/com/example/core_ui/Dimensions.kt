a1cd0a3a53ecd507dc2ca627f7050eb0836c5450
Uthman Ayinde <uthmanayinde6@gmail.com>
2022-04-04 11:37:25 +0100

    set up multi module and migrated to kotlin dsl

---
+++ core-ui/src/main/java/com/example/core_ui/Dimensions.kt
@@ -0,0 +1,9 @@
+package com.example.core_ui
+
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Dimensions(
+    val default: Dp = 0.dp,
+
+)
---
+++ onboarding/onboarding_presentation/src/main/java/com/example/onboarding_presentation/welcome/WelcomeScreen.kt
@@ -0,0 +1,19 @@
+package com.example.onboarding_presentation.welcome
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun WelcomeScreen(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+
+    }
+}
